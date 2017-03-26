91a7871cf3508cee3dfc1f1a8f8e2f3c3f116fbb
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-03-26 21:13:08 +0200

    Add CatagoryItem-table

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_dance.consumer.backend.sqla import (
     OAuthConsumerMixin,
-    )
+)
 from flask_login import (
     UserMixin,
 )
@@ -67,4 +67,47 @@ class Catagory(db.Model):
             'name': self.name,
             'description': self.description,
             'created_by_user_id': self.created_by_user_id,
+        }
+
+
+class CatagoryItem(db.Model):
+    """Catagory-items-table."""
+    __tablename__ = 'catagory-items'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(500))
+    created_by_user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    catagory_id = db.Column(db.Integer, db.ForeignKey(Catagory.id))
+    time_created = db.Column(db.DateTime, default=db.func.now())
+    catagory = db.relationship(Catagory)
+    user = db.relationship(User)
+
+    @classmethod
+    def create_catagory_item(
+            cls, name, description, created_by_user_id, catagory_id):
+        """Create a catagory-item."""
+        if not (2 < len(name) < 30):
+            raise ValueError(
+                "Name should be between 2 and 30 characters")
+        if not (3 < len(description) < 500):
+            raise ValueError(
+                "Description should be between 3 and 30 characters")
+        cat_item = CatagoryItem(
+            name=name,
+            description=description,
+            created_by_user_id=created_by_user_id,
+            catagory_id=catagory_id
+        )
+        db.session.add(cat_item)
+        db.session.commit()
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'created_by_user_id': self.created_by_user_id,
+            'catagory': self.catagory_id,
         }
